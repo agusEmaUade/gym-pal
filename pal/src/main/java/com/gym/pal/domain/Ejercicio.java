2fc57c244f299b2be48b304845ebdf2cafd421ed
lucasgballesta <112784286+lucasgballesta@users.noreply.github.com>
2024-11-04 20:07:31 -0300

    ejercicio

--- pal/src/main/java/com/gym/pal/domain/Ejercicio.java
+++ pal/src/main/java/com/gym/pal/domain/Ejercicio.java
@@ -1,5 +1,13 @@
 package com.gym.pal.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
 public class Ejercicio {
     private String nombre;
     private int repeticiones;
@@ -8,4 +16,33 @@ public class Ejercicio {
     private String exigencia_muscular;
     // private file video;
     private Nivel nivel;
+
+
+
+
+
+
+    public void mostrarDetalles() {
+        System.out.println("Ejercicio: " + nombre);
+        System.out.println("Repeticiones: " + repeticiones);
+        System.out.println("Series: " + series);
+        System.out.println("Peso asignado: " + peso_asignado + " kg");
+        System.out.println("Exigencia muscular: " + exigencia_muscular);
+        System.out.println("Nivel: " + nivel);
+        System.out.println("------------------------------");
+    }
+
+    public static void main(String[] args) {
+
+        Ejercicio sentadilla = new Ejercicio("Sentadilla", 12, 4, 60, "Piernas", Nivel.BAJO);
+
+        Ejercicio pressDeBanca = new Ejercicio("Press de Banca", 10, 3, 80, "Pectorales", Nivel.ALTO);
+
+        Ejercicio curlDeBiceps = new Ejercicio("Curl de Bíceps", 15, 4, 20, "Bíceps", Nivel.MEDIO);
+
+        // Mostrar detalles de los ejercicios
+        sentadilla.mostrarDetalles();
+        pressDeBanca.mostrarDetalles();
+        curlDeBiceps.mostrarDetalles();
+    }
 }
--- pal/src/main/java/com/gym/pal/repository/EjercicioRepository.java
+++ pal/src/main/java/com/gym/pal/repository/EjercicioRepository.java
@@ -1,12 +1,13 @@
 package com.gym.pal.repository;
 
 import com.gym.pal.domain.Ejercicio;
+import com.gym.pal.domain.Nivel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class EjercicioRepository {
-    private List<Ejercicio> ejercicios = new ArrayList<>();
+    private List<Ejercicio> ejercicios = initEjercicio();
 
     public List<Ejercicio> getAllSocio() {
         return ejercicios;
@@ -15,5 +16,14 @@ public class EjercicioRepository {
     public Ejercicio createUser(Ejercicio ejercicio) {
         ejercicios.add(ejercicio);
         return ejercicio;
+    }
+    private List<Ejercicio> initEjercicio(){
+        Ejercicio sentadilla = new Ejercicio("Sentadilla", 12, 4, 60, "Piernas", Nivel.BAJO);
+
+        Ejercicio pressDeBanca = new Ejercicio("Press de Banca", 10, 3, 80, "Pectorales", Nivel.ALTO);
+
+        Ejercicio curlDeBiceps = new Ejercicio("Curl de Bíceps", 15, 4, 20, "Bíceps", Nivel.MEDIO);
+
+        return List.of(sentadilla, pressDeBanca, curlDeBiceps);
     }
 }
